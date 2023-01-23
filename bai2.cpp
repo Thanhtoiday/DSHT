fc1f4973c1d046e0c7be25f63e4bcf57ea3eb89f
Thanhtoiday <thanh92351@st.vimaru.edu.vn>
2023-01-23 20:12:08 +0700

    Them bai 2

---
+++ bai2.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+#include<cmath>
+#include<cstring>
+
+using namespace std;
+
+class BN{
+    string ten;
+    int ma;
+    string benh;
+    string Khoa;
+    public:
+        BN(){
+            ten = "";
+            ma =0;
+            benh ="";
+            Khoa = "";
+        }
+
+        virtual void nhap();
+        virtual void xuat();
+        virtual int GetMa(){
+            return ma;
+        }
+};
+
+void BN::nhap(){
+    cin>>ten;
+    cin>>ma;
+    cin>>benh;
+    cin>>Khoa;
+}
+
+void BN::xuat(){
+    cout<<this->ten<<this->ma<<this->benh<<this->Khoa<<endl;
+}
+
+class NoiTru: public BN{
+    int maphong;
+    public:
+        NoiTru(){
+            maphong = 0;
+        }
+        void nhap(){
+            BN::nhap();
+            cin>>maphong;
+        }
+
+        void xuat(){
+            BN::xuat();
+            cout<<" "<<maphong;
+        }
+};
+
+class NgoaiTru: public BN{
+    string donthuoc;
+    public:
+        NgoaiTru(){
+            donthuoc = "";
+        }
+        void nhap(){
+            BN::nhap();
+            cin>>donthuoc;
+        }
+        void xuat(){
+            BN::xuat();
+            cout<<" "<<donthuoc;
+        }
+};
+
+
+
+int main(){
+    BN **ds;
+    int n;
+    cout<<"Nhap n: ";
+    cin>>n;
+    ds = new BN*[n];
+    int i;
+    for(i = 0; i<n ; i++){
+        cout<<"1-Noi"<<endl;
+        cout<<"2-Ngoai"<<endl;
+        int op;
+        cin>>op;
+        if(op == 1){
+            ds[i] = new NoiTru();
+        }
+        else if(op ==2){
+            ds[i] = new NgoaiTru();
+        }
+        ds[i]->nhap();
+    }
+
+    
+
+    for(int i=0; i<n-1; i++){
+        for(int j=i+1; j<n; j++){
+            if(ds[i]->GetMa() > ds[j]->GetMa()){
+                BN *tg = ds[i];
+                ds[i] = ds[j];
+                ds[j] = tg;
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        ds[i]->xuat();
+    }
+}
